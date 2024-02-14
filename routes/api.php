bdb6cf0c60a8dff3fcaccbf8475ac60db2799cd1
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-02-14 18:14:37 +0300

    api category/subcategory controllers

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\SubcategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::prefix('categories')->group(function(){
+    Route::get('/', [CategoryController::class, 'index']);
+    Route::get('/{category:id}', [SubcategoryController::class, 'index']);
 });
