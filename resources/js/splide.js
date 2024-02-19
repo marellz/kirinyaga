280c9aae95f0ca205c23b04259fa8b2f534c10de
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-02-19 16:10:10 +0300

    homepage slider

--- resources/js/splide.js
+++ resources/js/splide.js
@@ -1,22 +1,50 @@
-import { Splide } from '@splidejs/splide'
+import { Splide } from "@splidejs/splide";
+const sliders = [
+    {
+        el: ".splide.product-images",
+        options: {
+            type: "loop",
+            perPage: 3,
+            padding: {
+                left: "2rem",
+                right: "2rem",
+            },
+            breakpoints: {
+                640: {
+                    type: "loop",
+                    perPage: 1,
+                    padding: 30,
+                },
+                768: {
+                    type: "loop",
+                    perPage: 2,
+                    padding: 30,
+                },
+            },
+        },
+    },
+    {
+        el: ".splide.home-slider",
+        options: {
+            type: "loop",
+            autoWidth: true,
+            arrows:false,
+            pagination: false,
+        },
+    },
+];
 
-new Splide(".splide.product-images", {
-    type: "loop",
-    perPage: 3,
-    padding: {
-        left: "2rem",
-        right: "2rem",
-    },
-    breakpoints: {
-        640: {
-            type: "loop",
-            perPage: 1,
-            padding: 30,
-        },
-        768: {
-            type: "loop",
-            perPage: 2,
-            padding: 30,
-        },
-    },
-}).mount();
+const splides = [];
+
+sliders.forEach((slider) => {
+    if (document.querySelectorAll(slider.el).length) {
+        const instance = new Splide(slider.el, slider.options);
+        splides.push(instance);
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    splides.forEach((element) => {
+        element.mount();
+    });
+});
