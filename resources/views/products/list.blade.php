8fe02bbd6176f470806201ce1a706c57bd6c44da
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-02-21 11:01:31 +0300

    wip: filters on list page

--- resources/views/products/list.blade.php
+++ resources/views/products/list.blade.php
@@ -1,27 +1,36 @@
 <x-main-layout>
     <x-layout.container>
-        <x-layout.banner>
-            <div class="flex justify-between">
-                <x-type.page-title>
-                    All products
-                </x-type.page-title>
-
-                <a href="{{ route('products.create')}}">
-                    <x-custom.button>Create new</x-custom.button>
-                </a>
+        @php
+            $category_id = request()->get('category_id');
+            $subcategory_id = request()->get('subcategory_id');
+        @endphp
+        <div class="grid grid-cols-4 py-20 gap-20">
+            <div class="space-y-5 stick top-0">
+                <h1 class="font-bold text-lg text-gray-600">Filters</h1>
+                <form action="{{ route('products')}}">
+                    <div class="space-y-5">
+                        <product-category-loader category-id="{{ $category_id }}"
+                            subcategory-id="{{ $subcategory_id }}"></product-category-loader>
+                        <x-custom.button>Apply filters</x-custom.button>
+                    </div>
+                </form>
             </div>
-        </x-layout.banner>
-
-        <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-10">
-            @foreach ($products as $item)
-                <a href="{{ route('product.show', [$item]) }}">
-                    <x-product.card :product="$item" />
-                </a>
-            @endforeach
+            <div class="col-span-3 space-y-10">
+                <div class="flex justify-between items-center">
+                    <h1 class="text-xl font-bold">Products</h1>
+                    <form-input name="search" placeholder="Search">
+                </div>
+                <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-10">
+                    @foreach ($products as $item)
+                        <a href="{{ route('product.show', [$item]) }}">
+                            <x-product.card :product="$item" />
+                        </a>
+                    @endforeach
+                </div>
+                <div class="flex justify-center py-10">
+                    {{ $products->links() }}
+                </div>
+            </div>
         </div>
-        <div class="flex justify-center py-10">
-             {{ $products->links()}}
-        </div>
-
     </x-layout.container>
 </x-main-layout>
