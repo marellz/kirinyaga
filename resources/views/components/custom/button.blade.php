903260b56d9e3bb9f732cb03798c11c86d715fb5
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-03-07 15:14:58 +0300

    new icons, update button to allow icons

--- resources/views/components/custom/button.blade.php
+++ resources/views/components/custom/button.blade.php
@@ -1,10 +1,10 @@
-@props(['variant' => 'primary'])
+@props(['variant' => 'primary', 'icon' => null])
 @php
     $variants = [
         'primary' => ' bg-red-crayola border-transparent text-white hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-indigo-500',
         'secondary' => 'border bg-gray-200 border-gray-300 hover:bg-gray-300 focus:ring-indigo-500 disabled:opacity-25',
         'danger' => 'border border-transparent bg-red-600  text-white uppercase hover:bg-red-500 active:bg-red-700 focus:ring-red-500',
-    ];
+];
 @endphp
 
 <button
@@ -12,7 +12,11 @@
         'type' => 'submit',
         'class' => 'inline-flex items-center rounded-md font-semibold text-sm focus:ring-2 focus:outline-none focus:ring-offset-2 px-4 py-2 transition ease-in-out duration-150'. $variants[$variant],
     ]) }}>
+    
     <span>
         {{ $slot }}
     </span>
+    @if($icon)
+    <x-dynamic-component :component="$icon" size="w-4 h-4" />
+    @endif
 </button>
