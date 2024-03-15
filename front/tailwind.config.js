ca2ce004021de399b4eaca2d7d5458abafc13230
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-03-15 13:46:33 +0300

    vue > nuxt : futureproof

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,23 +1,31 @@
 /** @type {import('tailwindcss').Config} */
-import defaultTheme from 'tailwindcss/defaultTheme'
-import forms from '@tailwindcss/forms'
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 export default {
-  content: [],
-  theme: {
-    fontFamily: {
-      sans: ['Noto Sans', ...defaultTheme.fontFamily.sans]
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Noto Sans", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                gunmetal: "#1b2f33ff",
+                "light-cyan": "#cbeef3ff",
+                "tea-rose-red": "#eec6caff",
+                "red-crayola": "#ef3054ff",
+                madder: "#a41623ff",
+            },
+        },
     },
-    extend: {
-      colors: {
-        gunmetal: '#1b2f33ff',
-        'light-cyan': '#cbeef3ff',
-        'tea-rose-red': '#eec6caff',
-        'red-crayola': '#ef3054ff',
-        madder: '#a41623ff'
-      }
-    }
-  },
-  plugins: [forms]
+
+    plugins: [forms],
 }
 
