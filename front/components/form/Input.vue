926e3b75378a9ee2c4b0fb9e4b22fc899860c37c
Dave Njoroge <d.njoroge@thecodingmachine.com>
2024-03-15 18:52:19 +0300

    move components to nuxt

--- front/components/form/Input.vue
+++ front/components/form/Input.vue
@@ -1,25 +1,27 @@
 <template>
-    <div class="flex flex-col">
-        <label v-if="label" :for="id" class="block font-medium text-sm text-gray-700 mb-2">
-            {{ label }}
-        </label>
-        <input
-            :type="type"
-            :id="id"
-            :name="name"
-            class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-            :disabled="disabled"
-            :required="required"
-            :class="{'border-red-500': error}"
-            :placeholder="placeholder"
-            :autocomplete="autocomplete"
-            v-model="model"
-        />
-
-        <form-error v-if="error">
-            {{  error }}
-        </form-error>
-    </div>
+    <client-only>
+        <div class="flex flex-col">
+            <label v-if="label" :for="id" class="block font-medium text-sm text-gray-700 mb-2">
+                {{ label }}
+            </label>
+            <input
+                :type="type"
+                :id="id"
+                :name="name"
+                class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+                :disabled="disabled"
+                :required="required"
+                :class="{'border-red-500': error}"
+                :placeholder="placeholder"
+                :autocomplete="autocomplete"
+                v-model="model"
+            />
+    
+            <form-error v-if="error">
+                {{  error }}
+            </form-error>
+        </div>
+    </client-only>
 </template>
 <script lang="ts" setup>
 import { onMounted } from 'vue';
---
+++ front/components/layout/title.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1 class="text-3xl font-bold">
+    <slot />
+  </h1>
+</template>
+<script lang="ts" setup></script>
